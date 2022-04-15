0e26030124eb277d17ad320815337856f8dd58fd
Mathilda Holmström <mathilda.holmstrom@telia.com>
2022-04-15 11:51:25 +0200

    If applied, will add base of testing files.

---
+++ test/unit/test_get_user_by_email.py
@@ -0,0 +1,16 @@
+from backend.src.controllers.usercontroller import UserController
+
+
+class MockUser:
+    pass
+
+
+class TestGetUserByEmail:
+    def test_valid_email_user_exist(self):
+        pass
+
+    def test_valid_email_user_not_exist(self):
+        pass
+
+    def test_invalid_email_user_exist(self):
+        pass
