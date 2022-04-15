f17c62776fad2c417d81aec3ed6c74869f9edd79
Mathilda Holmström <mathilda.holmstrom@telia.com>
2022-04-15 15:47:16 +0200

    If applied, will add all testing for get_user_by_email function.

--- test/unit/test_get_user_by_email.py
+++ test/unit/test_get_user_by_email.py
@@ -1,25 +1,46 @@
-from unittest.mock import MagicMock
-
+from unittest.mock import MagicMock, patch, call
 import pytest
-
 from backend.src.controllers.usercontroller import UserController
-from backend.src.util.dao import DAO
-
-
-class MockUser:
-    pass
 
 
 class TestGetUserByEmail:
-    def test_valid_email_user_exist(self, mock_find):
+    def test_valid_email_one_user_exist(self):
         fake_dao = MagicMock()
-        fake_dao.find
-        uc = UserController()
-        valid_email = ''
-        pass
+        fake_dao.find.return_value = [{'_id': {'$oid': '6259697356df8080e997364d'}, 'email': 'jim@gmail.com',
+                                       'firstName': 'Jim', 'lastName': 'Ahlstrand'}]
+        uc = UserController(fake_dao)
+        valid_email = 'jim@gmail.com'
+
+        assert len(uc.get_user_by_email(valid_email)) != 0
+
+    @patch('builtins.print')
+    def test_valid_email_many_user_exist(self, mock_print):
+        fake_dao = MagicMock()
+        fake_dao.find.return_value = [{'_id': {'$oid': '6259697356df8080e997364d'}, 'email': 'jim@gmail.com',
+                                       'firstName': 'Jim', 'lastName': 'Ahlstrand'},
+                                      {'_id': {'$oid': '625969d956df8080e997364e'}, 'email': 'jim@gmail.com',
+                                       'firstName': 'Therese', 'lastName': 'Bergöö'}]
+        uc = UserController(fake_dao)
+        valid_email = 'jim@email.com'
+        returned_user = {'_id': {'$oid': '6259697356df8080e997364d'}, 'email': 'jim@gmail.com',
+                         'firstName': 'Jim', 'lastName': 'Ahlstrand'}
+        uc.get_user_by_email(valid_email)
+        mock_print.assert_called()
+
+        assert uc.get_user_by_email(email=valid_email) == returned_user
 
     def test_valid_email_user_not_exist(self):
-        pass
+        fake_dao = MagicMock()
+        fake_dao.find.return_value = {'users': []}
+        uc = UserController(fake_dao)
+
+        valid_email = 'valid@email.com'
+
+        # Should return none but raises error
+        assert uc.get_user_by_email(email=valid_email) is None
+
+        # with pytest.raises(Exception):
+        #     uc.get_user_by_email(email=valid_email)
 
     def test_invalid_email(self):
         uc = UserController(MagicMock())
