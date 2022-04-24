d7e21b51e22dcf5d7878d70e170bdbff7b5f1ed8
Mathilda Holmström <mathilda.holmstrom@telia.com>
2022-04-24 14:45:45 +0200

    If applied, test database at port 27018 has been initialized.

--- test/integration/test_dao_create.py
+++ test/integration/test_dao_create.py
@@ -4,11 +4,34 @@ from backend.src.controllers.usercontroller import UserController
 
 
 class TestDaoCreate:
-    def test_valid_email_one_user_exist(self):
-        fake_dao = MagicMock()
-        fake_dao.find.return_value = [{'_id': {'$oid': '6259697356df8080e997364d'}, 'email': 'jim@gmail.com',
-                                       'firstName': 'Jim', 'lastName': 'Ahlstrand'}]
-        uc = UserController(fake_dao)
-        valid_email = 'jim@gmail.com'
+    def test_string_uniqueItem_false(self):
+        # uniqueItem is for the tasks only since its the only array in the list.
+        dao_user = DAO('user')
+        user_not_unique = {'id': '625969d956df8080e997398e', 'email': "john.doe@test.com", 'firstName': "John",
+                           'lastName': 'Johnsson', 'tasks': [1, 2, 2]}
 
-        assert len(uc.get_user_by_email(valid_email)) != 0
+        with pytest.raises(Exception):
+            dao_user.create(user_not_unique)
+
+    def test_string_uniqueItem_true(self):
+        pass
+
+    def test_boolean_uniqueItem_false(self):
+        pass
+
+    def test_boolean_uniqueItem_true(self):
+        pass
+
+    # Returns object
+    def test_data_string_uniqueItems_true_all_props_true(self):
+        pass
+
+    def test_data_boolean_uniqueItems_true_all_props_true(self):
+        pass
+
+    # Should fail
+    def test_data_string_uniqueItems_false_all_props_true(self):
+        pass
+
+    def test_data_string_uniqueItems_true_all_props_false(self):
+        pass
