f8d5cc7b4feb8e35d577bdafc2b5a350ce1480b5
Mathilda Holmström <mathilda.holmstrom@telia.com>
2022-04-25 16:54:37 +0200

    If applied, final changes.

--- test/integration/test_dao_create.py
+++ test/integration/test_dao_create.py
@@ -1,7 +1,9 @@
-from unittest.mock import MagicMock, patch, call
+import json
+import os
+from unittest.mock import patch, MagicMock
 import pytest
 from backend.src.util.dao import DAO
-from backend.src.controllers.usercontroller import UserController
+from backend.src.util.validators import getValidator
 
 """     
 Creates a new document in the collection associated to this data access object. The creation of a new
@@ -30,101 +32,127 @@ In particular, the validator has to make sure that:
 
 
 @pytest.fixture
-def dao():
-    return DAO('user')
+def sut():
+    fabricated_filename = 'C:\\Users\\mathi\\Documents\\edutask\\backend\src\static\\validators\\test.json'
+    fabricated_data = {
+        "$jsonSchema": {
+            "bsonType": "object",
+            "required": ["firstName", "lastName", "email"],
+            "properties": {
+                "firstName": {
+                    "bsonType": "string",
+                    "description": "the first name of a user must be determined"
+                },
+                "lastName": {
+                    "bsonType": "string",
+                    "description": "the last name of a user must be determined"
+                },
+                "email": {
+                    "bsonType": "string",
+                    "description": "the email address of a user must be determined"
+                },
+                "tasks": {
+                    "bsonType": "array",
+                    "uniqueItems": True,
+                    "items": {
+                        "bsonType": "objectId"
+                    }
+                }
+            }
+        }
+    }
 
+    with open(fabricated_filename, 'w') as output_file:
+        json.dump(fabricated_data, output_file)
 
-@pytest.fixture
-def adding_valid_user(dao):
-    user = dao.create({'email': 'lbg@telia.com', 'firstName': 'Linnea', 'lastName': 'B'})
-    yield user
-    dao.delete(user['_id']['$oid'])
+    dao = DAO('test')
+    yield dao
+    dao.drop()
+    os.remove(fabricated_filename)
 
 
 # Only testing the validation for users.
 class TestDaoCreate:
     # the values of a property flagged with 'uniqueItems' are unique among all documents of the collection.
-    def test_string_uniqueItem_false(self):
+    def test_string_uniqueItem_false(self, sut):
         # uniqueItem is for the tasks only since its the only array in the list.
-        dao_user = DAO('user')
         tasks_not_unique = {'id': '625969d956df8080e997398e', 'email': "john.doe@test.com", 'firstName': "John",
                             'lastName': 'Johnsson', 'tasks': [1, 2, 2]}
         tasks_not_unique_two = {'id': '625969d956df8080e997398e', 'email': "john.doe@test.com", 'firstName': "John",
                                 'lastName': 'Johnsson', 'tasks': ['test', 'test', 'hello']}
 
         with pytest.raises(Exception):
-            dao_user.create(tasks_not_unique)
+            sut.create(tasks_not_unique)
 
         with pytest.raises(Exception):
-            dao_user.create(tasks_not_unique_two)
+            sut.create(tasks_not_unique_two)
 
     # (2) every property complies to the bson data type constraint
-    def test_firstname_not_comply(self):
-        dao_user = DAO('user')
+    def test_firstname_not_comply(self, sut):
         invalid_firstname_num = {'email': "john.doe@test.com", 'firstName': 123, 'lastName': 'Doe'}
         invalid_firstname_bool = {'email': "john.doe@test.com", 'firstName': False, 'lastName': 'Doe'}
         invalid_firstname_array = {'email': "john.doe@test.com", 'firstName': [1, 2, 3], 'lastName': 'Doe'}
 
         with pytest.raises(Exception):
-            dao_user.create(invalid_firstname_num)
+            sut.create(invalid_firstname_num)
 
         with pytest.raises(Exception):
-            dao_user.create(invalid_firstname_bool)
+            sut.create(invalid_firstname_bool)
 
         with pytest.raises(Exception):
-            dao_user.create(invalid_firstname_array)
+            sut.create(invalid_firstname_array)
 
-    def test_lastname_not_comply(self):
-        dao_user = DAO('user')
+    def test_lastname_not_comply(self, sut):
         invalid_lastname_num = {'email': "john.doe@test.com", 'firstName': 'Jane', 'lastName': 123}
         invalid_lastname_bool = {'email': "john.doe@test.com", 'firstName': 'Jane', 'lastName': True}
         invalid_lastname_array = {'email': "john.doe@test.com", 'firstName': 'Jane', 'lastName': [1, 2, 3]}
 
         with pytest.raises(Exception):
-            dao_user.create(invalid_lastname_num)
+            sut.create(invalid_lastname_num)
 
         with pytest.raises(Exception):
-            dao_user.create(invalid_lastname_bool)
+            sut.create(invalid_lastname_bool)
 
         with pytest.raises(Exception):
-            dao_user.create(invalid_lastname_array)
+            sut.create(invalid_lastname_array)
 
-    def test_email_not_comply(self):
-        dao_user = DAO('user')
+    def test_email_not_comply(self, sut):
         invalid_email_num = {'email': 123, 'firstName': 'Jane', 'lastName': 'Doe'}
         invalid_email_bool = {'email': False, 'firstName': 'Jane', 'lastName': 'Doe'}
         invalid_email_array = {'email': [1, 2, 3], 'firstName': 'Jane', 'lastName': 'Doe'}
 
         with pytest.raises(Exception):
-            dao_user.create(invalid_email_num)
+            sut.create(invalid_email_num)
 
         with pytest.raises(Exception):
-            dao_user.create(invalid_email_bool)
+            sut.create(invalid_email_bool)
 
         with pytest.raises(Exception):
-            dao_user.create(invalid_email_array)
+            sut.create(invalid_email_array)
 
-    def test_tasks_not_comply(self):
+    def test_tasks_not_comply(self, sut):
         pass
 
     # (1) the data for the new object contains all required properties
-    def test_does_not_have_all_required_properties(self):
-        dao_user = DAO('user')
+    def test_does_not_have_all_required_properties(self, sut):
         no_lastname = {'email': 'test@test.com', 'firstName': 'Jane'}
         no_firstname = {'email': 'test@test.com', 'lastName': 'Doe'}
         no_email = {'firstName': 'Jane', 'lastName': 'Doe'}
 
         with pytest.raises(Exception):
-            dao_user.create(no_lastname)
+            sut.create(no_lastname)
 
         with pytest.raises(Exception):
-            dao_user.create(no_firstname)
+            sut.create(no_firstname)
 
         with pytest.raises(Exception):
-            dao_user.create(no_email)
+            sut.create(no_email)
 
-    # Returns object, all 1-3 are good.
-    def test_data_string_uniqueItems_true_all_props_true(self, adding_valid_user, dao):
-        user = adding_valid_user
+    # Returns object, all 1-3 conditions are good.
+    def test_data_string_uniqueItems_true_all_props_true(self, sut):
+        user_data = {'email': 'lbg@telia.com', 'firstName': 'Linnea', 'lastName': 'B'}
+        user = sut.create(user_data)
 
-        assert dao.findOne(user['_id']['$oid']) == user
+        assert sut.findOne(user['_id']['$oid'])['email'] == user_data['email']
+        assert sut.findOne(user['_id']['$oid'])['firstName'] == user_data['firstName']
+        assert sut.findOne(user['_id']['$oid'])['lastName'] == user_data['lastName']
