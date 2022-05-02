a87353b1d145ddf57338f8ffadc2b827367308c9
Mathilda Holmström <mathilda.holmstrom@telia.com>
2022-05-02 16:03:52 +0200

    If applied, adds comments to show which table the it-tests are for.

--- frontend/cypress/integration/R8UC1-3/user_task_testing.js
+++ frontend/cypress/integration/R8UC1-3/user_task_testing.js
@@ -69,19 +69,26 @@ describe('Users task testing', () => {
         tearDown()
     })
 
+    // R8UC1 - USER ENTERS A DESCRIPTION
     it('should allow user to type in a description', () => {
+        // Check that the description contains the correct value after type
         cy.get('.inline-form').find('input[type=text]').type('Test description')
             .should("contain.value", 'Test description')
     });
 
-    it('should allow the user to create new todo item when description is not empty', () => {
+    // R8UC1 - PRESSES ADD BUTTON
+    it('should allow the user to create new todo item when description is NOT empty', () => {
         // Check length of the list is +1 after form is submitted
+        cy.get('.inline-form').find('input[type=text]').type('Test description')
+            .should("contain.value", 'Test description')
     });
 
     it('should NOT allow the user to create new todo item when description is empty', () => {
         // Check so that the length is the same/expected value
     });
 
+
+    // R8UC2 - CLICKS ON ICON IN FRONT OF THE DESCRIPTION
     it('should make the todo item border red if its set to done', () => {
         // Check after a red border
     });
@@ -102,9 +109,8 @@ describe('Users task testing', () => {
         // Check if text is not struck through
     });
 
+    // R8UC3 - CLICKS ON THE X SYMBOL BEHIND THE DESCRIPTION
     it('should remove todo from list if task is deleted', () => {
         // Check that the length of the list is -1 or the expected value.
     });
-
-
 })
